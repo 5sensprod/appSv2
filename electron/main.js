176797a64980c76d7cf8a9c1bc0ca1c972464224
chris <5sensprod@gmail.com>
2024-10-03 02:06:06 +0200

    clean lib

--- electron/main.js
+++ electron/main.js
@@ -1,11 +1,8 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const { SerialPort } = require('serialport'); // Modification ici
-
-let serialPort;
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 // Vérification manuelle si on est en mode développement
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === 'development'
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
@@ -17,66 +14,29 @@ function createWindow() {
       nodeIntegration: false,
       sandbox: false,
     },
-  });
+  })
 
   if (isDev) {
     // En mode développement, charger depuis le serveur React
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL('http://localhost:3000')
   } else {
     // En production, charger les fichiers compilés de React
-    mainWindow.loadFile(path.join(__dirname, '../frontend/build/index.html'));
+    mainWindow.loadFile(path.join(__dirname, '../frontend/build/index.html'))
   }
 }
 
 app.whenReady().then(() => {
-  // Ouvrir le port série
-  serialPort = new SerialPort({
-    path: 'COM10', // Notez que l'API a changé pour nécessiter un objet de configuration
-    baudRate: 9600, // Ajustez selon votre appareil
-  });
-
-  serialPort.on('open', () => {
-    console.log('Port série ouvert');
-  });
-
-  serialPort.on('error', (err) => {
-    console.error('Erreur du port série :', err.message);
-  });
-
-  createWindow();
+  createWindow()
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+      createWindow()
     }
-  });
-});
-
-// Gestion de l'événement 'write-serial'
-ipcMain.handle('write-serial', async (event, message) => {
-  if (serialPort && serialPort.isOpen) {
-    return new Promise((resolve, reject) => {
-      serialPort.write(message + '\n', (err) => {
-        if (err) {
-          console.error('Erreur lors de l\'écriture sur le port série :', err.message);
-          reject(err);
-        } else {
-          console.log('Message envoyé :', message);
-          resolve('Message envoyé avec succès');
-        }
-      });
-    });
-  } else {
-    console.error('Le port série n\'est pas ouvert');
-    throw new Error('Le port série n\'est pas ouvert');
-  }
-});
+  })
+})
 
 app.on('window-all-closed', () => {
-  if (serialPort && serialPort.isOpen) {
-    serialPort.close();
-  }
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
